5208555193bc472e4348fbf99c24e8a7a2778d2c
hfors <hforsatgit@gmail.com>
2018-03-17 17:37:59 +0100

    Complete data read functionality

--- examples/handwritten-digits.cpp
+++ examples/handwritten-digits.cpp
@@ -8,24 +8,45 @@
 #include <iostream>
 #include <fstream>
 
+#include <intrin.h>
+
 enum class DataType {Images, Labels};
 
-Eigen::MatrixXd readData(std::ifstream& file, DataType dt);
+Eigen::MatrixXd readData(std::ifstream& file, DataType dt, const std::int32_t maxItems = -1);
 
 int main()
 {
-	std::ifstream file("../testdata/train-images.idx3-ubyte", std::ios::binary);
-	if (!file) {
+	const std::int32_t maxTrainItems = 10000;
+	const std::int32_t maxTestItems = 2000;
+
+	std::ifstream trainInputFile("../testdata/train-images.idx3-ubyte", std::ios::binary);
+	if (!trainInputFile) {
 		std::cout << "Couldnt open data file";
 	}
+	Eigen::MatrixXd trainInput = readData(trainInputFile, DataType::Images, maxTrainItems);
 
-	Eigen::MatrixXd trainImages = readData(file, DataType::Images);
-	
+	std::ifstream trainOutputFile("../testdata/train-labels.idx1-ubyte", std::ios::binary);
+	if (!trainOutputFile) {
+		std::cout << "Couldnt open data file";
+	}
+	Eigen::MatrixXd trainOutput = readData(trainOutputFile, DataType::Labels, maxTrainItems);
+
+	std::ifstream testInputFile("../testdata/t10k-images.idx3-ubyte", std::ios::binary);
+	if (!testInputFile) {
+		std::cout << "Couldnt open data file";
+	}
+	Eigen::MatrixXd testInput = readData(testInputFile, DataType::Images, maxTestItems);
+
+	std::ifstream testOutputFile("../testdata/t10k-labels.idx1-ubyte", std::ios::binary);
+	if (!testOutputFile) {
+		std::cout << "Couldnt open data file";
+	}
+	Eigen::MatrixXd testOutput = readData(testOutputFile, DataType::Labels, maxTestItems);
 
 	return 0;
 }
 
-Eigen::MatrixXd readData(std::ifstream& file, DataType dt)
+Eigen::MatrixXd readData(std::ifstream& file, DataType dt, const std::int32_t maxItems)
 {
 	Eigen::MatrixXd data;
 
@@ -34,18 +55,18 @@ Eigen::MatrixXd readData(std::ifstream& file, DataType dt)
 
 		std::int32_t magicNum = 0;
 		file.read((char*)&magicNum, 4);
-		std::cout << std::hex << magicNum << std::endl;
+		magicNum = _byteswap_ulong(magicNum);
 
 		std::int32_t nitems = 0;
 		file.read((char*)&nitems, 4);
-		std::cout << nitems << std::endl;
+		nitems = _byteswap_ulong(nitems);
 
 		std::int32_t nrows = 0, ncols = 0;
 		if (dt == DataType::Images) {
 			file.read((char*)&nrows, 4);
 			file.read((char*)&ncols, 4);
-			std::cout << nitems << std::endl;
-			std::cout << nitems << std::endl;
+			nrows = _byteswap_ulong(nrows);
+			ncols = _byteswap_ulong(ncols);
 		}
 		else /*if (dt == DataType::Labels) */ {
 			nrows = 10;
@@ -53,19 +74,31 @@ Eigen::MatrixXd readData(std::ifstream& file, DataType dt)
 		}
 		data.resize(nrows, ncols*nitems);
 		data.setZero();
+
+		if (maxItems > 0 && nitems > maxItems) {
+			nitems = maxItems;
+		}
 		
 		unsigned char element;
 		for (int k = 0; k < nitems; ++k) {
 			for (int i = 0; i < nrows; ++i) {
 				for (int j = 0; j < ncols; ++j) {
 					file.read((char*)&element, sizeof(element));
-					data(i, j + k * ncols) = element;
+					if (dt == DataType::Images) {
+						data(i, j + k * ncols) = element;
+					}
+					else /*if (dt == DataType::Labels) */ {
+						data(element, k*ncols) = 1;
+						i = nrows;
+					}
+
 				}
 			}
-			if (k % 1000 == 0) {
-				std::cout << k << " items read.\n";
+			if ((k + 1) % 1000 == 0) {
+				std::cout << '\r' << ((k + 1)*100) / nitems << "% of items read.";
 			}
 		}
+		std::cout << std::endl;
 	}
 	catch (std::ifstream::failure&) {
 		std::cout << "Error reading file\n";
